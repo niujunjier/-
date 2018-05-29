a6e529539f0f321a6a4e95ecd7eda689162df5e2
niujj <15156158076@163.com>
2018-05-29 19:58:41 +0800

    提问

--- pages/answer/answer.js
+++ pages/answer/answer.js
@@ -1,7 +1,16 @@
 // pages/answer/answer.js
+const app = getApp();
 Page({
   data: {
-    classId: ''
+    classId: '',
+    first: true,
+    url: '',
+    sendRedStatu: false,
+    title: {
+      name: '提问',
+      description: ''
+    },
+    isPlay: false
   },
   onLoad: function (options) {
     this.setData({ classId: options.classId || 234})
@@ -12,11 +21,25 @@ Page({
     wx.onSocketOpen(function (res) {
       console.log('WebSocket连接已打开！')
       console.log(res)
+      wx.sendSocketMessage({
+        data: '{ "Action": "login", "RoomId": "' + self.data.classId + '", "User": { "id": "' + app.globalData.code + '","name": "' + app.globalData.name + '","signed": "teacher"} }'
+      })
     })
     wx.onSocketError(function (res) {
       console.log('WebSocket连接打开失败，请检查！')
     })
     wx.onSocketMessage(function (res) {
+      if(self.data.first){
+        wx.sendSocketMessage({
+          data: '{ "Action": "question", "RoomId": "' + self.data.classId + '", "User": {  } }'
+        })
+        self.setData({ first: false })
+      }else{
+        let list = JSON.parse(res.data);
+        if (list.Action == 'question' && list.User.url) {
+          self.setData({ url: list.User.url, sendRedStatu: true })
+        }
+      }
       console.log(res)
     })
   },
@@ -24,5 +47,24 @@ Page({
     wx.navigateTo({
       url: '/pages/classForSt/classForSt?classId=' + this.data.classId,
     })
+  },
+  sendAsw(e){
+    console.log(e.target.dataset.key)
+    wx.sendSocketMessage({
+      data: '{ "Action": "question", "RoomId": "' + this.data.classId + '", "User": { "asw": "' + e.target.dataset.key +'" } }'
+    })
+    this.setData({ sendRedStatu: false })
+  },
+  test(){
+    wx.sendSocketMessage({
+      // data: '{ "Action": "question", "RoomId": "' + this.data.classId + '", "User": { "asw": "a" } }'
+      data: '{ "Action": "question", "RoomId": "' + this.data.classId + '", "User": { "url": "https://pan.baidu.com/s/1eRJ9Gps" } }'
+    })
+  },
+  showMaskToggle(){
+    this.setData({ sendRedStatu: false })
+  },
+  onUnload(){
+    wx.closeSocket();
   }
 })
--- pages/record/record.js
+++ pages/record/record.js
@@ -3,7 +3,14 @@ const app = getApp();
 Page({
   data: {
     isRecording: false,
-    classId: ''
+    classId: '',
+    isFirst: true,
+    count: {
+      a: 0,
+      b: 0,
+      c: 0,
+      d: 0
+    }
   },
   onLoad: function (options) {
     this.setData({ classId: options.classId })
@@ -23,13 +30,16 @@ Page({
   },
   sendQuestion() {
     let self = this;
+    if (!self.data.isFirst) {
+      return;
+    }
     wx.connectSocket({
       url: 'wss://juplus.cn:9502'
     })
     wx.onSocketOpen(function (res) {
       console.log('WebSocket连接已打开！')
       wx.sendSocketMessage({
-        data: '{ "Action": "question", "RoomId": "' + self.data.classId + '", "User": { "url": "https://pan.baidu.com/s/1eRJ9Gps" } }'
+        data: '{ "Action": "login", "RoomId": "' + self.data.classId + '", "User": { "id": "' + app.globalData.code + '","name": "' + app.globalData.name + '","signed": "teacher"} }'
       })
     })
     wx.onSocketError(function (res) {
@@ -37,9 +47,33 @@ Page({
     })
     wx.onSocketMessage(function (res) {
       console.log(res)
+      if (self.data.isFirst) {
+        wx.sendSocketMessage({
+          data: '{ "Action": "question", "RoomId": "' + self.data.classId + '", "User": { "url": "https://pan.baidu.com/s/1eRJ9Gps" } }'
+        })
+        self.setData({ isFirst: false })
+      } else {
+        let count = {a: 0,b: 0,c: 0,d: 0};
+        let list = JSON.parse(res.data);
+        // list.forEach(function (ele) {
+        //   let item = JSON.parse(ele)
+        //   if (item.Action == 'question') {
+        //     count[item.User.asw] ++;
+        //   }
+        // })
+        if (list.Action == 'question' && list.User.asw){
+          count[list.User.asw]++;
+        }
+        self.setData({ count: count });
+      }
     })
   },
-  clearQuestion(){
+  clearQuestion() {
+    this.setData({ isFirst: true })
+    this.setData({ count: { a: 0, b: 0, c: 0, d: 0 } });
+    wx.closeSocket();
+  },
+  onUnload(){
     wx.closeSocket();
   }
 })
